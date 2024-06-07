8f104210510b0ff56467d710b33eea0144777692
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2024-06-06 22:03:52 -0500

    update: application

--- src/main/java/com/groweasy/userservice/UserserviceApplication.java
+++ src/main/java/com/groweasy/userservice/UserserviceApplication.java
@@ -1,13 +1,36 @@
 package com.groweasy.userservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @SpringBootApplication
 public class UserserviceApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+						.allowedHeaders("*");
+			}
+		};
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
-
 }
